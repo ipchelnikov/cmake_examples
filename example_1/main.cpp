18b03459c84db864ee4d9ae6fb0f0751a5094c4e
Igor Pchelnikov <gopchelnikov@gmail.com>
2024-01-06 18:04:02 +0300

    Add example 1

---
+++ example_1/main.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <memory>
+
+
+#include "semple_config.h"
+
+int main(int argc, char* argv[])
+{
+    printf("%s(%d.%d)\n", argv[0], Semple_VERSION_MAJOR, Semple_VERSION_MINOR);
+
+    int a = 0;
+    auto addr = std::to_address(&a); // Use c++20 feature, to check it is available
+    printf("Addr: %p", addr);
+    
+}
